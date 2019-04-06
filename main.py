0cbecbc7f492732a26b4d6dd3fcac6ecd350a953
Ben Yang <benayang@gmail.com>
2019-04-05 23:56:37 -0700

    switch back to redis

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import json
-import sqlite3
+# import sqlite3
 from pathlib import Path
+from math import inf
 
 import unicodedataplus as unicodedata
-# import redis
+import redis
 from jinja2 import Template
 from sanic import Sanic, response
 import ideograph
@@ -13,18 +14,15 @@ app = Sanic()
 
 app.static("/static", "./static")
 
-conn = sqlite3.connect("cmp-log.db")
-cursor = conn.cursor()
-# r = redis.Redis()
-# component_counter = Counter(json.load(open("component_counter.json")))
-
-def create_cmp_log():
-    cursor.execute("CREATE TABLE cmp_log (cmp text PRIMARY KEY, num int)")
+try:
+    r = redis.from_url(os.environ.get("REDIS_URL"))
+except ValueError:
+    r = redis.Redis()
 
 def stroke_sort(c):
     s = unicodedata.total_strokes(c)
     if s == 0:
-        return 255
+        return inf
     else:
         return s
 
@@ -35,18 +33,11 @@ async def main(request):
     except KeyError:
         components = ""
     for comp in components:
-        # r.zincrby("cmps", 1, comp)
-        cursor.execute("INSERT OR IGNORE INTO cmp_log VALUES (?, 0)", comp)
-        cursor.execute("UPDATE cmp_log SET num = (num + 1) WHERE cmp = ?", comp)
-    conn.commit()
+        r.zincrby("cmps", 1, comp)
     template = Template(open("form.jinja2").read())
     ideos = sorted(ideograph.find(components), key=stroke_sort)
     ideographs = [(i, "".join(sorted(ideograph.components(i), key=stroke_sort))) for i in ideos]
-    # try:
-    #     common_components = [c.decode("utf-8") for c in r.zrevrange("cmps", 0, -1)]
-    # except ConnectionError:
-    cursor.execute("SELECT cmp FROM cmp_log ORDER BY num DESC")
-    common_components = [c[0] for c in cursor.fetchall()]
+    common_components = [c.decode("utf-8") for c in r.zrevrange("cmps", 0, -1)]
     return response.html(template.render(
         components=components,
         ideographs=ideographs,
